1c4736842d89349a63584e7c4f2b09c3cff1aca2
Erling Hjermstad <erling-hjermstad@hotmail.com>
2025-03-25 21:06:22 +0100

    more favicons

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,9 +17,21 @@ app.include_router(api.router)
 async def serveIcon():
     return FileResponse("./frontend/dist/favicon.ico")
 
+@app.get("/site.webmanifest", include_in_schema=False)
+async def serveManifest():
+    return FileResponse("./frontend/dist/serve.webmanifest")
+
+@app.get("/apple-touch-icon.png", include_in_schema=False)
+async def serveAppleIcon():
+    return FileResponse("./frontend/dist/apple-touch-icon.png")
+
+@app.get("/android-chrom-192x192.ico", include_in_schema=False)
+async def serveAndroidIcon():
+    return FileResponse("./frontend/dist/android-chrome-192x192.png")
+
 # Catch-all route for SPA - must be last
 @app.get("/{full_path:path}", include_in_schema=False)
 async def catch_all(full_path: str):
     if not full_path.startswith("api/"):
         return FileResponse("./frontend/dist/index.html")
-    raise HTTPException(status_code=404, detail="Not found") 
+    raise HTTPException(status_code=404, detail="Not found")
